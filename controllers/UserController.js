e8755d3f85c2f0ab8cd85ab583fe4c56bd2844e5
SGBM-Jimbo <76476900+SGBM-Jimbo@users.noreply.github.com>
2023-01-09 03:11:25 -0600

    Update UserController.js
    Let's see if this fixes the issue.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -185,7 +185,7 @@ router.post("/login", oAuth, async (req, res) => {
       httpOnly: true,
       maxAge: 2592000000,
       sameSite: true,
-      Domain: "zauartcc.org",
+      domain: "zauartcc.org",
     }); // Expires in 30 days
   } catch (e) {
     req.app.Sentry.captureException(e);
