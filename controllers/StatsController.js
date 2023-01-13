0f1933477e7fd48fa1f457a041035e8d368aae63
SGBM-Jimbo <76476900+SGBM-Jimbo@users.noreply.github.com>
2023-01-12 22:49:51 -0600

    Update StatsController.js
    Updates INS Dashboard to not show users with the zau cert. This will show everyone that does not have the zau cert.

--- controllers/StatsController.js
+++ controllers/StatsController.js
@@ -148,7 +148,7 @@ router.get('/ins', getUser, auth(['atm', 'datm', 'ta', 'ins', 'mtr', 'ia']), asy
 		const cidsWithTraining = lastTraining.map(train => train.studentCid);
 		const cidsWithoutTraining = allCids.filter(cid => !cidsWithTraining.includes(cid))
 
-		const controllersWithoutTraining = allHomeControllers.filter(c => cidsWithoutTraining.includes(c.cid));
+		const controllersWithoutTraining = allHomeControllers.filter((c) => cidsWithoutTraining.includes(c.cid)).filter(c => !c.certCodes.includes('zau'));
 		lastRequest = lastRequest.reduce((acc, cur) => {
 			acc[cur.studentCid] = cur
 			return acc;
